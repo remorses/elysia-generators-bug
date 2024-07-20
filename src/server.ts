459a672a000ab0bfa4643aad15c12c1a17a6cfc2
remorses <beats.by.morse@gmail.com>
2024-07-20 11:21:23 +0200

    more descriptive

--- src/server.ts
+++ src/server.ts
@@ -3,47 +3,59 @@ import { sleep } from 'bun'
 import { Elysia } from 'elysia'
 
 const app = new Elysia()
-    .get('/yieldedItemsBecomeAString', async function* generator() {
+    .get('/yielded-items-become-a-single-string', async function* generator() {
         yield JSON.stringify({ hello: 'world 1' })
         // if you add sleep(100) here it gets fixed
         yield JSON.stringify({ hello: 'world 2' })
     })
-    .get('/yieldedObjectsBecomeObjectObject', async function* generator() {
+    .get('/yielded-objects-become-object-object', async function* generator() {
         await sleep(100)
         yield { body: 'Hello Elysia' } // this will be [object Object]
     })
-    .get('/yieldedJsonStringsBecomeObjects', async function* generator() {
-        await sleep(100) // add sleep to fix string output bug
-        yield JSON.stringify({ body: 'Hello Elysia' }) // this will be the object {"hello":"world"} in Eden, should remain string instead
-    })
+    .get(
+        '/yielded-json-strings-become-objects-in-eden',
+        async function* generator() {
+            await sleep(100) // add sleep to fix string output bug
+            yield JSON.stringify({ body: 'Hello Elysia' }) // this will be the object {"hello":"world"} in Eden, should remain string instead
+        },
+    )
     .listen(3000, async () => {
         const app = treaty<App>('localhost:3000')
 
         {
-            const { data: gen, error } =
-                await app.yieldedItemsBecomeAString.get({})
+            const { data: gen, error } = await app[
+                'yielded-items-become-a-single-string'
+            ].get({})
             if (error) throw error
 
             for await (const res of gen) {
-                console.log('yieldedItemsBecomeAString', res)
+                console.log('yielded-items-become-a-single-string', res)
             }
         }
+        console.log()
         {
-            const { data: gen, error } =
-                await app.yieldedObjectsBecomeObjectObject.get({})
+            const { data: gen, error } = await app[
+                'yielded-objects-become-object-object'
+            ].get({})
             if (error) throw error
 
             for await (const res of gen) {
-                console.log('yieldedObjectsBecomeObjectObject', res)
+                console.log('yielded-objects-become-object-object', res)
             }
         }
+        console.log()
         {
-            const { data: gen, error } =
-                await app.yieldedJsonStringsBecomeObjects.get({})
+            const { data: gen, error } = await app[
+                'yielded-json-strings-become-objects-in-eden'
+            ].get({})
             if (error) throw error
 
             for await (const res of gen) {
-                console.log('yieldedJsonStringsBecomeObjects', res)
+                console.log(
+                    'this should be a string, instead it is an',
+                    typeof res,
+                )
+                console.log('yielded-json-strings-become-objects-in-eden', res)
             }
         }
     })
